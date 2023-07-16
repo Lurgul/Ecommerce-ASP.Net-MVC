1f2e2be770db64df3ef4069076049a446f086eb5
Nagy Richard <herold.humor99@gmail.com>
2023-07-16 21:51:22 +0200

    Configurating the Scope dependency-injection of the PlatformsServices.

---
+++ Data/Services/IPlatformsService.cs
@@ -0,0 +1,13 @@
+﻿using GameStore.Models;
+
+namespace GameStore.Data.Services
+{
+    public interface IPlatformsService
+    {
+        Task<IEnumerable<Platforms>> GetAll();
+        Platforms GetById(int id);
+        void Add(Platforms Platforms);
+        Platforms Update(int id, Platforms newPlatforms);
+        void Delete(int id);
+    }
+}
